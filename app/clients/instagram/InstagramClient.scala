5c0f025237898f8e590f262d248640d40409550d
Rushi Jash <rishijash@hotmail.com>
2019-09-01 20:05:01 -0700

    Instagram Client

--- app/clients/instagram/InstagramClient.scala
+++ app/clients/instagram/InstagramClient.scala
@@ -2,6 +2,7 @@ package clients.instagram
 
 import clients.Response
 import javax.inject.Inject
+import org.slf4j.LoggerFactory
 import play.api.Configuration
 import play.api.libs.json.Json
 import scalaj.http.{Http, HttpOptions}
@@ -13,6 +14,8 @@ class InstagramClient {
 
   val baseUrl = "https://www.instagram.com/"
 
+  private val log = LoggerFactory.getLogger(this.getClass.getName)
+
   def getProfile(username: String): Future[Option[InstagramProfile]] = {
     val profileUrl = getUsernameRequestUrl(username)
     sendRequest(profileUrl).map(response => {
@@ -23,13 +26,21 @@ class InstagramClient {
 
   private def sendRequest(url: String): Future[Option[Response]] = {
     Future {
-      val result = Http(url)
-        .header("Accept", "application/json")
-        .option(HttpOptions.readTimeout(4000)).asString
-      if (result.code == 200) {
-        Some(Response(result.code.toString, result.body))
-      } else {
-        None
+      try {
+        val result = Http(url)
+          .header("Accept", "application/json")
+          .option(HttpOptions.readTimeout(4000)).asString
+        if (result.code == 200) {
+          Some(Response(result.code.toString, result.body))
+        } else {
+          log.error(s"Error is getting profile from Instagram url: ${url}")
+          None
+        }
+      } catch {
+        case e: Exception => {
+          log.error(s"Error is getting profile from Instagram url: ${url} with Exception: ${e.getMessage}")
+          None
+        }
       }
     }
   }
