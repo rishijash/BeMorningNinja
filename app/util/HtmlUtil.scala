8fb7a2d8a677facde3749b74e2dbf21736ea8524
Rushi Jash <rishijash@hotmail.com>
2019-09-01 23:33:32 -0700

    test

--- app/util/HtmlUtil.scala
+++ app/util/HtmlUtil.scala
@@ -1,12 +1,14 @@
 package util
 
+import org.slf4j.LoggerFactory
 import scalaj.http.{Http, HttpOptions}
 
 import scala.concurrent.Future
-
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object HtmlUtil {
+
+  private val log = LoggerFactory.getLogger(this.getClass.getName)
 
   def getHtmlFromUrl(url: String): Future[Option[String]] = {
     sendRequest(url).map(htmlCode => {
@@ -18,9 +20,10 @@ object HtmlUtil {
     Future {
       val result = Http(url)
         .option(HttpOptions.readTimeout(2000)).asString
-      if (result.code == 200) {
+      if (result.is2xx) {
         Some(result.body)
       } else {
+        log.error(s"Error calling Url: ${url} with response: ${result}")
         None
       }
     }
