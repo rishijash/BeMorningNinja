f6a4d13b9baf0bd0df3ce8cac577553b6ab608ee
Rushi Jash <rishijash@hotmail.com>
2019-09-22 19:27:27 -0700

    Modal Rules

--- web-assets/clock_alarm.js
+++ web-assets/clock_alarm.js
@@ -198,6 +198,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
             submit.innerText = "CANCEL";
 
+            triggered = false;
+
             // disable stuff
             $('#carouselRow').css("pointer-events","none");
             $("#alarm_hours").prop("disabled", true);
@@ -394,7 +396,10 @@ function renderAccountList(accounts, slider) {
 }
 
 $(document).ready(function(){
-    $('.modal').modal();
+    $('.modal').modal({
+        dismissible: false
+    });
+
 
     // Hide on mobile
     var mobile = (/iphone|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
